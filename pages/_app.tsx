3fc1065a75ef475a4416e33f16839e16f558a3f3
w3cdp6084-dev <yusukew3@gmail.com>
2022-07-29 07:25:40 +0900

    _app import Header

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react'
 import { extendTheme } from '@chakra-ui/react';
 import '../styles/globals.scss'
-import Header from '../components/Header';
+import {Header} from '../components/Header';
 import Footer from '../components/Footer';
 const colors = {
   brand: {
