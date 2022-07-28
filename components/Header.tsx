0a7ecb7806012c5886185a027371c55e91e968b6
w3cdp6084-dev <yusukew3@gmail.com>
2022-07-29 07:31:10 +0900

    Header tag delete

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,10 @@
 import Link from 'next/link'
 import { FC } from 'react';
-
 import {
     Box,
     Flex,
     Container,
     Heading,
-    /* 追加 */
     useColorMode,
     useColorModeValue,
     Button
@@ -16,23 +14,19 @@ import { MoonIcon, SunIcon } from '@chakra-ui/icons';
 export const Header:FC= () => {
     const { colorMode, toggleColorMode } = useColorMode();
   return (
-    <header className="text-gray-600 body-font bg-blue-500">
         <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
             <Container maxW="container.lg">
                 <Flex as="header" py="4" justifyContent="space-between" alignItems="center">
                     <Link href="/" passHref>
-                        {/* 変更 ライトモードでgray.600、ダークモードでwhiteとする */}
                         <Heading as='h1' fontSize="2xl" cursor="pointer" color={useColorModeValue('gray.600', 'white')}>
                             Next.js microCMS Blog
                         </Heading>
                     </Link>
-                    {/* 追加 切り替えアイコン */}
                     <Button size='lg' onClick={toggleColorMode}>
                         {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
                     </Button>
                 </Flex>
             </Container>
         </Box>
-    </header>
   )
 }
