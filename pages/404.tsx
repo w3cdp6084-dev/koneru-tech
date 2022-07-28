cb607b42ffd0231d391bbc5aea77be92e0cf69bd
w3cdp6084-dev <yusukew3@gmail.com>
2022-07-29 06:59:30 +0900

    404 pages

---
+++ pages/404.tsx
@@ -0,0 +1,10 @@
+export default function Custom404() {
+    return (
+      <main className="main">
+        <div className="">
+          <h2 className="u-title-404">404</h2>
+          <p className="text-center">🐱ページがありません🐱</p>
+        </div>
+      </main>
+    );
+  }
