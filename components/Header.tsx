6806bf4d4a4b22e2d5bb58a5c11308b85c6e71f3
w3cdp6084-dev <yusukew3@gmail.com>
2022-07-29 07:23:02 +0900

    Header with chakraUI

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,20 +1,38 @@
 import Link from 'next/link'
+import { FC } from 'react';
 
+import {
+    Box,
+    Flex,
+    Container,
+    Heading,
+    /* 追加 */
+    useColorMode,
+    useColorModeValue,
+    Button
+} from '@chakra-ui/react';
+import { MoonIcon, SunIcon } from '@chakra-ui/icons';
 
-
-export default function Header() {
+export const Header:FC= () => {
+    const { colorMode, toggleColorMode } = useColorMode();
   return (
     <header className="text-gray-600 body-font bg-blue-500">
-      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <Link href={'/'} passHref>
-          <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-            <span className="ml-3 text-xl text-white">My Blog</span>
-          </a>
-        </Link>
-        <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-          <a className="mr-5 text-white hover:text-gray-900">Profile</a>
-        </nav>
-      </div>
+        <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
+            <Container maxW="container.lg">
+                <Flex as="header" py="4" justifyContent="space-between" alignItems="center">
+                    <Link href="/" passHref>
+                        {/* 変更 ライトモードでgray.600、ダークモードでwhiteとする */}
+                        <Heading as='h1' fontSize="2xl" cursor="pointer" color={useColorModeValue('gray.600', 'white')}>
+                            Next.js microCMS Blog
+                        </Heading>
+                    </Link>
+                    {/* 追加 切り替えアイコン */}
+                    <Button size='lg' onClick={toggleColorMode}>
+                        {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
+                    </Button>
+                </Flex>
+            </Container>
+        </Box>
     </header>
   )
 }
