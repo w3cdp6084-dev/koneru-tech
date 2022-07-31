58864b090426365ae74e1b05d41b17b518cb40ab
w3cdp6084-dev <yusukew3@gmail.com>
2022-07-31 15:00:24 +0900

    index => chakra

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,35 +1,37 @@
 import { client } from '../src/libs/client';
 import type { Blog } from '../types/blog';
 import Link from 'next/link';
+import {
+  Box,
+} from '@chakra-ui/react';
+
 type Props = {
   blogs: Array<Blog>;
 };
 
 export default function Home({blogs}: Props) {
   return(
-    <div>
-    <section>
-    <h1>人気の記事😽</h1>
-    <div>
-      {blogs.map(blogs => (
-      <Link href={`/blogs/${blogs.id}`} passHref>
-        <div className="dark:bg-gray-800 rounded-xl overflow-hidden shadow-lg" key={blogs.id}>
-          <img
-            className="w-full"
-            src={blogs.eye_catch.url}
-            alt="Sunset in the mountains"
-          />
+    <Box>
+      <section>
+        <h1>人気の記事😽</h1>
+        <div>
+          {blogs.map(blogs => (
+          <Link href={`/blogs/${blogs.id}`} passHref>
+            <div key={blogs.id}>
+              <img
+                src={blogs.eye_catch.url}
+                alt="test"
+              />
+            </div>
+          </Link>
+          ))}
         </div>
-      </Link>
-      ))}
-    </div>
-  </section>
-  </div>
+      </section>
+    </Box>
   )
 }
 export const getServerSideProps = async () => {
   const data = await client.get({ endpoint: 'blog' });
-
   return {
     props: {
       blogs: data.contents,
