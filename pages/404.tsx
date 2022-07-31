15aa41a2c82e2d8e332aa8f8211191c40fb9b943
w3cdp6084-dev <yusukew3@gmail.com>
2022-07-31 13:56:14 +0900

    404 styling

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,9 +1,15 @@
+import styles from '../styles/pages/404.module.scss'
+
+import {
+  Box,
+
+} from '@chakra-ui/react';
 export default function Custom404() {
     return (
-      <main className="main">
-        <div className="">
-          <h2 className="u-title-404">404</h2>
-          <p className="text-center">🐱ページがありません🐱</p>
+      <main className={styles.main}>
+        <div className={styles.item}>
+          <h2 className={styles.titleH2}>404</h2>
+          <p className={styles.desc}>🙀ページがありません🙀</p>
         </div>
       </main>
     );
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,12 +1,25 @@
+import Image from 'next/image'
 import styles from '../styles/pages/about.module.scss'
+
 import {
   Box,
+
 } from '@chakra-ui/react';
-export default function NotFound() {
+export default function About() {
     return (
       <main className={styles.main}>
-        <Box className={styles.item}>
-        🙀404🙀
+        <Box className={styles.itemWrap}>
+          <Image src="/images/logo.png" width={150} height={150} alt="logo" />
+          <p className={styles.aboutNickname}>KONEKONE</p>
+          <p className={styles.aboutName}>
+            Yusuke Mori🐱<br />
+            Designer/Developer🍳
+          </p>
+          <p className={styles.aboutDesc}>
+          KONEKONEという屋号で、デザイナーとフロントエンドをやってます。<br />
+          ブランディング/コンセプトメイクの設計を大切にし、モノづくりをおこなっています。<br />
+          少しでも参考になれば幸いです😸
+          </p>
         </Box>
       </main>
     );
