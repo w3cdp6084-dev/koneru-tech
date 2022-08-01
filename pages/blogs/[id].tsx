d8992b75e51a82d681acbb7f35762cd71cd26f43
w3cdp6084-dev <yusukew3@gmail.com>
2022-08-01 11:38:46 +0900

    目次 styling 日付

--- pages/blogs/[id].tsx
+++ pages/blogs/[id].tsx
@@ -17,12 +17,10 @@ export default function Blog({ blogs }: Props) {
   return (
     <div className={styles.wrap}>
       <Flex className={styles.inner}>
-        <div className="max-w-6xl px-10 py-6 mx-auto bg-gray-50">
-          <div className="mt-2">
-            <div className="sm:text-3xl md:text-3xl lg:text-3xl xl:text-4xl font-bold text-blue-500">
+        <div className={styles.contentLeft}>
+            <div className={styles.blogTitle}>
               {blogs.title}
             </div>
-          </div>
           {blogs.tag && (
             <div className="flex items-center justify-start mt-4 mb-4">
               <div className="px-2 py-1 font-bold bg-red-400 text-white rounded-lg">
@@ -39,7 +37,7 @@ export default function Blog({ blogs }: Props) {
             src={blogs.eye_catch.url}
           />
         </div>
-        <div>
+        <div className={styles.toc}>
         {blogs.toc_visible && (
           <TableOfContents toc={toc} />
           )}
